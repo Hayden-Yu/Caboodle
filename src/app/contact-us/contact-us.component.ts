0b5592d3f18467d1556dd6a000b26efab0f74d0b
Mark Wong <markwong14@hotmail.com>
2018-06-19 22:12:20 -0400

    fixed contact us page a bit, but submit still doesn't work

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styles: []
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  private rForm : FormGroup;
+  private post:any;
+  private fName:string = '';
+  private lName:string = '';
+  private username: string = '';
+  private email: string = '';
+  private issue:string = '';
+  private message:string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { 
+      this.rForm = fb.group({
+        'fName': [null, Validators.required],
+        'lName': [null, Validators.required],
+        'issue': [null, Validators.required],
+        'message': [null, Validators.required],
+        
+      })
   }
-
+  ngOnInit(){}
+  addPost(post){
+    this.fName = post.fName;
+    this.lName = post.lName;
+  }
 }
