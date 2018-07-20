1de4c42c64a90ed2bfc5ad9f3a5620088bc41b81
Mark Wong <markwong14@hotmail.com>
2018-07-20 16:02:21 -0400

    fixed contact us validation alerts, added about us blurb, surrounded blurb and team in about us with bubble, rm'ed api create zombie

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {  FormBuilder, FormGroup, Validators} from '@angular/forms';
-import * as nodemailer from 'nodemailer';
+
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
@@ -20,7 +20,7 @@ export class ContactUsComponent {
       this.rForm = fb.group({
         'fName': [null, Validators.required],
         'lName': [null, Validators.required],
-        'email': [null, Validators.email],
+        'email': [null, Validators.compose([Validators.required, Validators.email])],
         'issue': [null, Validators.required],
         'message': [null, Validators.compose([Validators.required, Validators.minLength(10),Validators.maxLength(400)])]
         
@@ -32,19 +32,6 @@ export class ContactUsComponent {
     this.email = post.email;
     this.issue = post.issue;
     this.message = post.message;
- /*   let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      secure: false,
-      port: 25,
-      auth:{
-        user: 'CaboodleAPI123@gmail.com',
-        pass: '12345678'
 
-      },
-      tls:{
-        rejectUnauthorized: false
-      }
-    });*/
-  
   }
 }
